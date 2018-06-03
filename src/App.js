d02138618306a8f41aee5a0459d8ed820b194d38
丰硕 <fengshuochn@126.com>
2018-06-04 00:14:06 +0800

    feat: add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import './App.css';
+import Home from './views/Home.js'
+import List from './views/List.js'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <Router>
+        <div>
+          <Route exact path="/" component={Home}></Route>
+          <Route path="/list" component={List}></Route>
+        </div>
+      </Router>
+    )
   }
 }
 
